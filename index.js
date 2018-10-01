5cc7a086a0c3970b48232d94d8df6bfa31cfb93b
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-01 11:15:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-function happyHolidays() {
- return "Happy holidays!"
+function happyHolidays(){
+  return "Happy holidays!";
 }
-
+//Write a function that takes in a parameter
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
-
-function happyHolidaysTo(holiday,name){
+//Write a function that takes in multiple parameters
+function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`;
 }
-
+//Write a function that takes in multiple parameters
 function holidayCountdown(holiday, days){
   return `It's ${days} days until ${holiday}!`;
 }
